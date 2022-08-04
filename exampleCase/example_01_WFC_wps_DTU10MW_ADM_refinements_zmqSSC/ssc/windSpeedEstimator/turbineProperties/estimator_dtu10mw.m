efc29aba57325e6fbabdcdf47eec534569f16421
Jean Gonzalez Silva <j.gonzalezsilva@tudelft.nl>
2022-08-04 14:38:38 +0200

    Update SOWFA simulation files

--- exampleCase/example_01_WFC_wps_DTU10MW_ADM_refinements_zmqSSC/ssc/windSpeedEstimator/turbineProperties/estimator_dtu10mw.m
+++ exampleCase/example_01_WFC_wps_DTU10MW_ADM_refinements_zmqSSC/ssc/windSpeedEstimator/turbineProperties/estimator_dtu10mw.m
@@ -1,13 +1,15 @@
 function [turbineProperties] = estimator_dtu10mw()
 % NEW (information straight from SOWFA):
 load('cpInterp_DTU10MW_SOWFA.mat')
+load('ctInterp_DTU10MW_SOWFA.mat')
 
 % Define turbine properties
 turbineProperties = struct(...
     'gearboxRatio',50.0,... % Gearbox ratio [-]
     'inertiaTotal',1.409969209E+08,... % Total inertia
     'rotorRadius',89.2,... % Rotor radius [m]
+	'ctFun',ctInterpolant,...
     'cpFun',cpInterpolant,... % Cp interpolant for TSR and blade pitch
+	'GBEfficiency',1,...
     'fluidDensity',1.23); % Fluid density
-	'GBEfficiency',1.0); % Gear box efficiency
 end
